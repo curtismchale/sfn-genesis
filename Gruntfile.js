4625d22b26dba80db016bcc1a019763c88b97994
Curtis McHale <curtis@curtismchale.ca>
2016-01-20 11:10:28 -0800

    initial project commit

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+	  dist: {
+		  options: {
+			style: 'compressed'
+		  },
+		  files: {
+			'style.css': 'style.scss',
+		  }
+		}
+	},
+	watch: {
+		sass: {
+			files: ['style.scss', 'sass/*.scss'],
+			tasks: ["sass"]
+		},
+	}
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['sass']);
+
+};
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# SFNdesign Genesis Starter Theme
+
+## .01
+
+Really just the genesis sample theme with Sass
